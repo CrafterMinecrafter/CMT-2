f377476ad28563d23318f74fcc0e37b4519bf392
Даниил <67530305+CrafterMinecrafter@users.noreply.github.com>
2020-09-04 14:38:45 +0000

    10
    Aes and Utils Class added

---
+++ CMT 2/BS/AES.cs
@@ -0,0 +1,321 @@
+﻿using System;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+
+
+namespace CMT_2.BS
+{
+    public static class AesEncryptor
+    {
+
+        static AesEncryptor()
+        {
+            AesEncryptor.aes.BlockSize = 128;
+        }
+
+
+        public static byte[] GenerateIV()
+        {
+            AesEncryptor.aes.GenerateIV();
+            return AesEncryptor.aes.IV;
+        }
+
+
+        public static byte[] Encrypt(byte[] buffer)
+        {
+            AesEncryptor.aes.GenerateIV();
+            byte[] result;
+            using (ICryptoTransform cryptoTransform = AesEncryptor.aes.CreateEncryptor())
+            {
+                byte[] second = cryptoTransform.TransformFinalBlock(buffer, 0, buffer.Length);
+                result = AesEncryptor.aes.IV.Concat(second).ToArray<byte>();
+            }
+            return result;
+        }
+
+
+        public static byte[] Decrypt(byte[] buffer)
+        {
+            byte[] rgbIV = buffer.Take(16).ToArray<byte>();
+            byte[] result;
+            using (ICryptoTransform cryptoTransform = AesEncryptor.aes.CreateDecryptor(AesEncryptor.aes.Key, rgbIV))
+            {
+                result = cryptoTransform.TransformFinalBlock(buffer, 16, buffer.Length - 16);
+            }
+            return result;
+        }
+
+
+        public static byte[] EncryptIV(byte[] buffer, byte[] IV)
+        {
+            return AesEncryptor.EncryptKeyIV(buffer, AesEncryptor.aes.Key, IV);
+        }
+
+
+        public static byte[] DecryptIV(byte[] buffer, byte[] IV)
+        {
+            return AesEncryptor.DecryptKeyIV(buffer, AesEncryptor.aes.Key, IV);
+        }
+
+
+        public static byte[] EncryptKeyIV(byte[] buffer, byte[] key, byte[] IV)
+        {
+            byte[] result;
+            using (ICryptoTransform cryptoTransform = AesEncryptor.aes.CreateEncryptor(key, IV))
+            {
+                result = cryptoTransform.TransformFinalBlock(buffer, 0, buffer.Length);
+            }
+            return result;
+        }
+
+
+        public static byte[] DecryptKeyIV(byte[] buffer, byte[] key, byte[] IV)
+        {
+            byte[] result;
+            using (ICryptoTransform cryptoTransform = AesEncryptor.aes.CreateDecryptor(key, IV))
+            {
+                result = cryptoTransform.TransformFinalBlock(buffer, 0, buffer.Length);
+            }
+            return result;
+        }
+
+
+        public static string Encrypt(string unencrypted)
+        {
+            return Convert.ToBase64String(AesEncryptor.Encrypt(AesEncryptor.encoder.GetBytes(unencrypted)));
+        }
+
+
+        [Obsolete("Decrypt(string) has been made obsolete. Please use the DecryptString(string).")]
+        public static string Decrypt(string encrypted)
+        {
+            return AesEncryptor.DecryptString(encrypted);
+        }
+
+
+        public static string DecryptString(string encrypted)
+        {
+            return AesEncryptor.DecryptString(Convert.FromBase64String(encrypted));
+        }
+
+
+        public static string DecryptString(byte[] encrypted)
+        {
+            byte[] array = AesEncryptor.Decrypt(encrypted);
+            return AesEncryptor.encoder.GetString(array, 0, array.Length);
+        }
+
+
+        public static string EncryptIV(string unencrypted, byte[] vector)
+        {
+            return Convert.ToBase64String(AesEncryptor.EncryptIV(AesEncryptor.encoder.GetBytes(unencrypted), vector));
+        }
+
+
+        public static string DecryptIV(string encrypted, byte[] vector)
+        {
+            byte[] array = AesEncryptor.DecryptIV(Convert.FromBase64String(encrypted), vector);
+            return AesEncryptor.encoder.GetString(array, 0, array.Length);
+        }
+
+
+        public static string Encrypt(bool unencrypted)
+        {
+            return Convert.ToBase64String(AesEncryptor.Encrypt(BitConverter.GetBytes(unencrypted)));
+        }
+
+
+        public static bool DecryptBool(string encrypted)
+        {
+            return AesEncryptor.DecryptBool(Convert.FromBase64String(encrypted));
+        }
+
+
+        public static bool DecryptBool(byte[] encrypted)
+        {
+            return BitConverter.ToBoolean(AesEncryptor.Decrypt(encrypted), 0);
+        }
+
+
+        public static string Encrypt(char unencrypted)
+        {
+            return Convert.ToBase64String(AesEncryptor.Encrypt(BitConverter.GetBytes(unencrypted)));
+        }
+
+
+        public static char DecryptChar(string encrypted)
+        {
+            return AesEncryptor.DecryptChar(Convert.FromBase64String(encrypted));
+        }
+
+
+        public static char DecryptChar(byte[] encrypted)
+        {
+            return BitConverter.ToChar(AesEncryptor.Decrypt(encrypted), 0);
+        }
+
+
+        public static string Encrypt(double unencrypted)
+        {
+            return Convert.ToBase64String(AesEncryptor.Encrypt(BitConverter.GetBytes(unencrypted)));
+        }
+
+
+        public static double DecryptDouble(string encrypted)
+        {
+            return AesEncryptor.DecryptDouble(Convert.FromBase64String(encrypted));
+        }
+
+
+        public static double DecryptDouble(byte[] encrypted)
+        {
+            return BitConverter.ToDouble(AesEncryptor.Decrypt(encrypted), 0);
+        }
+
+
+        public static string Encrypt(float unencrypted)
+        {
+            return Convert.ToBase64String(AesEncryptor.Encrypt(BitConverter.GetBytes(unencrypted)));
+        }
+
+
+        public static float DecryptFloat(string encrypted)
+        {
+            return AesEncryptor.DecryptFloat(Convert.FromBase64String(encrypted));
+        }
+
+
+        public static float DecryptFloat(byte[] encrypted)
+        {
+            return BitConverter.ToSingle(AesEncryptor.Decrypt(encrypted), 0);
+        }
+
+
+        public static string Encrypt(int unencrypted)
+        {
+            return Convert.ToBase64String(AesEncryptor.Encrypt(BitConverter.GetBytes(unencrypted)));
+        }
+
+
+        public static int DecryptInt(string encrypted)
+        {
+            return AesEncryptor.DecryptInt(Convert.FromBase64String(encrypted));
+        }
+
+
+        public static int DecryptInt(byte[] encrypted)
+        {
+            return BitConverter.ToInt32(AesEncryptor.Decrypt(encrypted), 0);
+        }
+
+
+        public static string Encrypt(long unencrypted)
+        {
+            return Convert.ToBase64String(AesEncryptor.Encrypt(BitConverter.GetBytes(unencrypted)));
+        }
+
+
+        public static long DecryptLong(string encrypted)
+        {
+            return AesEncryptor.DecryptLong(Convert.FromBase64String(encrypted));
+        }
+
+
+        public static long DecryptLong(byte[] encrypted)
+        {
+            return BitConverter.ToInt64(AesEncryptor.Decrypt(encrypted), 0);
+        }
+
+
+        public static string Encrypt(short unencrypted)
+        {
+            return Convert.ToBase64String(AesEncryptor.Encrypt(BitConverter.GetBytes(unencrypted)));
+        }
+
+
+        public static short DecryptShort(string encrypted)
+        {
+            return AesEncryptor.DecryptShort(Convert.FromBase64String(encrypted));
+        }
+
+
+        public static short DecryptShort(byte[] encrypted)
+        {
+            return BitConverter.ToInt16(AesEncryptor.Decrypt(encrypted), 0);
+        }
+
+
+        public static string Encrypt(uint unencrypted)
+        {
+            return Convert.ToBase64String(AesEncryptor.Encrypt(BitConverter.GetBytes(unencrypted)));
+        }
+
+
+        public static uint DecryptUInt(string encrypted)
+        {
+            return AesEncryptor.DecryptUInt(Convert.FromBase64String(encrypted));
+        }
+
+
+        public static uint DecryptUInt(byte[] encrypted)
+        {
+            return BitConverter.ToUInt32(AesEncryptor.Decrypt(encrypted), 0);
+        }
+
+
+        public static string Encrypt(ulong unencrypted)
+        {
+            return Convert.ToBase64String(AesEncryptor.Encrypt(BitConverter.GetBytes(unencrypted)));
+        }
+
+
+        public static ulong DecryptULong(string encrypted)
+        {
+            return AesEncryptor.DecryptULong(Convert.FromBase64String(encrypted));
+        }
+
+
+        public static ulong DecryptULong(byte[] encrypted)
+        {
+            return BitConverter.ToUInt64(AesEncryptor.Decrypt(encrypted), 0);
+        }
+
+
+        public static string Encrypt(ushort unencrypted)
+        {
+            return Convert.ToBase64String(AesEncryptor.Encrypt(BitConverter.GetBytes(unencrypted)));
+        }
+
+
+        public static ushort DecryptUShort(string encrypted)
+        {
+            return AesEncryptor.DecryptUShort(Convert.FromBase64String(encrypted));
+        }
+
+
+        public static ushort DecryptUShort(byte[] encrypted)
+        {
+            return BitConverter.ToUInt16(AesEncryptor.Decrypt(encrypted), 0);
+        }
+
+
+        private const int keySize = 16;
+
+
+        private const string keyString = "defaultKeyString";
+
+
+        private const int IvLength = 16;
+
+
+        private static UTF8Encoding encoder = new UTF8Encoding();
+
+
+        private static AesManaged aes = new AesManaged
+        {
+            Key = AesEncryptor.encoder.GetBytes("testKey").Take(16).ToArray<byte>()
+        };
+    }
+
+}
---
+++ CMT 2/BS/Utils.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace CMT_2.BS
+{
+
+    public class Utils
+    {
+        #region MD5
+        public string MD5(string text)
+        {
+            UTF8Encoding utf8Encoding = new UTF8Encoding();
+            byte[] bytes = utf8Encoding.GetBytes(text);
+            MD5CryptoServiceProvider md5CryptoServiceProvider = new MD5CryptoServiceProvider();
+            byte[] array = md5CryptoServiceProvider.ComputeHash(bytes);
+            string text2 = string.Empty;
+            for (int i = 0; i < array.Length; i++)
+            {
+                text2 += Convert.ToString(array[i], 16).PadLeft(2, '0');
+            }
+            return text2.PadLeft(32, '0');
+            #endregion
+        }
+        public string MD5File(string FilePath)
+        {
+            byte[] bytes = File.ReadAllBytes(FilePath);
+            MD5CryptoServiceProvider md5CryptoServiceProvider = new MD5CryptoServiceProvider();
+            byte[] array = md5CryptoServiceProvider.ComputeHash(bytes);
+            string text2 = string.Empty;
+            for (int i = 0; i < array.Length; i++)
+            {
+                text2 += Convert.ToString(array[i], 16).PadLeft(2, '0');
+            }
+            return text2.PadLeft(32, '0');
+        }
+
+        #region XOR
+        public string XOR(string text, string key, bool encrypt)
+        {
+            if (encrypt)
+            {
+                return Convert.ToBase64String(EncryptDecrypt(Encoding.UTF8.GetBytes(text), Encoding.UTF8.GetBytes(key)));
+            }
+            return Encoding.UTF8.GetString(EncryptDecrypt(Convert.FromBase64String(text), Encoding.UTF8.GetBytes(key)));
+        }
+        public string XORFile(string FilePath, string key, bool encrypt)
+        {
+            if (encrypt)
+            {
+                return Convert.ToBase64String(EncryptDecrypt(File.ReadAllBytes(FilePath), Encoding.UTF8.GetBytes(key)));
+            }
+            return Encoding.UTF8.GetString(EncryptDecrypt(Convert.FromBase64String(File.ReadAllText(FilePath)), Encoding.UTF8.GetBytes(key)));
+        }
+
+
+        private byte[] EncryptDecrypt(byte[] data, byte[] key)
+        {
+            int num = 0;
+            int num2 = 0;
+            int num3 = 0;
+            byte[] array = new byte[256];
+            for (int i = 0; i < 256; i++)
+            {
+                array[i] = (byte)i;
+            }
+            for (int j = 0; j < 256; j++)
+            {
+                num3 = (num3 + (int)array[j] + (int)key[j % key.Length]) % 256;
+                byte b = array[j];
+                array[j] = array[num3];
+                array[num3] = b;
+            }
+            byte[] array2 = new byte[data.Length];
+            for (int k = 0; k < data.Length; k++)
+            {
+                num = (num + 1) % 256;
+                num2 = (num2 + (int)array[num]) % 256;
+                byte b = array[num];
+                array[num] = array[num2];
+                array[num2] = b;
+                array2[k] = (byte)(data[k] ^ array[(int)(array[num] + array[num2]) % 256]);
+            }
+            return array2;
+        }
+        #endregion
+    }
+
+}
